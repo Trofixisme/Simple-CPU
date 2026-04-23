efe76c805e4e3e9a135d0e12966db87f7cf29102
Ziad <mziad039@gmail.com>
2026-04-23 14:38:26 +0200

    Fix

---
+++ Simple CPU/Instructions.h
@@ -0,0 +1,15 @@
+//
+//  FetchDecodeExecute.h
+//  Simple CPU
+//
+//  Created by Ziad on 23/04/2026.
+//
+
+#ifndef FetchDecodeExecute_h
+#define FetchDecodeExecute_h
+
+void fetch(void);
+void decode(void);
+void execute(void);
+
+#endif /* FetchDecodeExecute_h */
